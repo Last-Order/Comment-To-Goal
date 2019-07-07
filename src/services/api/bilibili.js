17574b06816978f90910c4c16bfba53e3a3bdce7
Eridanus Sora <sora@sound.moe>
2019-07-07 21:53:16 +0800

    fix bugs

--- src/services/api/bilibili.js
+++ src/services/api/bilibili.js
@@ -5,7 +5,7 @@ class Bilibili {
         return (await axios.get('https://api.live.bilibili.com/gift/v3/live/gift_config')).data.data;
     }
     static async getRoomGiftList(roomId, allGiftList) {
-        const GET_ROOM_GIFT_LIST_API = `https://api.live.bilibili.com/gift/v3/live/room_gift_list?roomid=${roomId}&platform=all`;
+        const GET_ROOM_GIFT_LIST_API = `https://api.live.bilibili.com/gift/v3/live/room_gift_list?roomid=${roomId}&area_v2_id=199&area_v2_parent_id=1&platform=all`;
         const response = (await axios.get(GET_ROOM_GIFT_LIST_API)).data.data;
         const roomGiftList = response.list.concat(response.old_list);
         if (allGiftList) {
